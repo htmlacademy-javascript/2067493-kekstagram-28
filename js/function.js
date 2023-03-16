57ff735ae28aa89bad1e74fcc9446a328719de83
Никита Межецкий <nsm020598@gmail.com>
2023-03-16 22:19:11 +1000

    Создаст функция для проверки строки на палидром

--- js/function.js
+++ js/function.js
@@ -7,8 +7,36 @@ function cheksLength (lineWord, maxLength) {
 
   return false;
 }
-
 console.log(cheksLength('Просто так', 18));
 console.log(cheksLength('Жизнь это хорошо', 5));
 console.log(cheksLength('Просто проверяем как работает', 8));
+
+
+
+//Функция для проверки строка палидромом
+//Для начала сделаем функцию которая развернет строку.
+function unwrapString (string) {
+  //Переведем все в один регистр и удалим все пробелы
+  let lowerString = string.toLowerCase().replaceAll(' ','');
+  let reverseString = '';
+  for (let i = lowerString.length - 1; i >= 0; i--){
+    reverseString += lowerString[i];
+  }
+  return reverseString;
+
+}
+// функция сравнения строк
+function cheksPalidrom (string){
+  let lowerString = string.toLowerCase().replaceAll(' ','');
+  if (lowerString === unwrapString(string)){
+    return true;
+  }
+
+  return false;
+
+}
+
+console.log(cheksPalidrom('ТоПот'));
+console.log(cheksPalidrom('Шопот'));
+console.log(cheksPalidrom('Молоко делили ледоколом'));
 
