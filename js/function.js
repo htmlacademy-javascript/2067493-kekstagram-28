369e1da0c1ff845172e5eb4ca4d80cad4339cb79
Никита Межецкий <nsm020598@gmail.com>
2023-03-17 21:09:57 +1000

    Создаст функцию для добавления символов

--- js/function.js
+++ js/function.js
@@ -17,7 +17,7 @@ console.log(cheksLength('Просто проверяем как работает
 //Для начала сделаем функцию которая развернет строку.
 function unwrapString (string) {
   //Переведем все в один регистр и удалим все пробелы
-  let lowerString = string.toLowerCase().replaceAll(' ','');
+  const lowerString = string.toLowerCase().replaceAll(' ','');
   let reverseString = '';
   for (let i = lowerString.length - 1; i >= 0; i--){
     reverseString += lowerString[i];
@@ -27,7 +27,7 @@ function unwrapString (string) {
 }
 // функция сравнения строк
 function cheksPalidrom (string){
-  let lowerString = string.toLowerCase().replaceAll(' ','');
+  const lowerString = string.toLowerCase().replaceAll(' ','');
   if (lowerString === unwrapString(string)){
     return true;
   }
@@ -43,11 +43,11 @@ console.log(cheksPalidrom('Молоко делили ледоколом'));
 // Функция которая извлекает число
 function findNumber(message) {
   //Если в сообщение будет число его необходимо преобразовать в строку
-  let messageString = String(message);
+  const messageString = String(message);
   // Определим переменную в которую будет записываться число
   let number = '';
   for (let i = 0; i <=messageString.length - 1; i++){
-    if(parseInt(messageString[i]) >= 0){
+    if(parseInt(messageString[i], 10) >= 0){
       number += messageString[i];
     }
     number += '';
@@ -57,4 +57,37 @@ function findNumber(message) {
 
 console.log (findNumber('Пусть будет такое число 1691 и еще вот такое 985 и такое 125'));
 console.log (findNumber('просто такие числа 12 и 85 и 985'));
-console.log (findNumber(12151));
+console.log (findNumber(-12.151));
+
+
+
+// Функция которая добавляет символы
+function addSimbol (originalString, minLengthString, additionString){
+  let newString = originalString;
+  //Количество дополнительных символов
+  let size = minLengthString - newString.length;
+
+  if (size <= additionString.length && size > 0){
+    newString = additionString.slice(0, size) + newString;
+    return newString
+  } else if(size > additionString.length){
+    while (size > 0){
+      if(size > additionString.length) {
+        newString = additionString.slice(0, additionString.length) + newString;
+        size = minLengthString - newString.length
+      } else {
+        newString = additionString.slice(0, size) + newString
+        return newString;
+      }
+    }
+  }
+
+  return newString;
+}
+
+console.log (addSimbol('1', 2, '0'));
+console.log (addSimbol('1', 4, '0'));
+console.log (addSimbol('q', 4, 'werty'));
+console.log (addSimbol('q', 4, 'we'));
+console.log (addSimbol('qwerty', 4, '0'));
+
