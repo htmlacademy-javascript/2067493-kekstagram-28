1f6b59061386270b2c5649e964a5b6553044c723
Никита Межецкий <nsm020598@gmail.com>
2023-04-08 02:27:35 +1000

    создаст файл который ищет рандомный элемент

--- js/main.js
+++ js/main.js
@@ -31,15 +31,7 @@ const DESCRIPTIONS = [
 ];
 
 // Функция которая дает рандомное число из задаваемого отрезка
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
 
-// функция для поиска случайного элемента в массиве
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 //Функция для формирования комментария к фотографии
 const getComments = (number) => {
